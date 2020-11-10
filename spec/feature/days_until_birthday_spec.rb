3ae22b7413a476422a2fd76c79b2578ea2e5d47f
Alex Withington-Smith <alex@awithsmith.com>
2020-11-10 13:12:12 +0000

    configured rackup, added test for the site to show if it's a users birthday

--- spec/feature/days_until_birthday_spec.rb
+++ spec/feature/days_until_birthday_spec.rb
@@ -1,5 +1,6 @@
 require "capybara/rspec"
 require_relative "../../app.rb"
+require "date"
 
 Capybara.app = DaysUntilBirthdayApp
 
@@ -10,5 +11,15 @@ feature "show basic web app" do
     expect(find_field('month'))
     expect(page).to have_content('Welcome to the Birthday Countdown!')
     expect(page).to have_button('Submit')
+  end
+end
+
+feature "tell user happy birthday if it's their birthday" do
+  scenario "show pleasant birthday greeting after user enters today's date" do
+    visit("/")
+    fill_in('day', with: Date.today.day)
+    fill_in('month', with: Date.today.month)
+    click_button "Submit"
+    expect(page).to have_content(/It's today! Many happy returns!/)
   end
 end
